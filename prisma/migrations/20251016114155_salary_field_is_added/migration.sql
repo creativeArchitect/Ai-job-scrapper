59e946cb6b0581fcb4aacc7932959fda2bd59e86
AyushAJS <jangidayush89@gmail.com>
2025-10-16 17:12:08 +0530

    updation in schema

---
+++ prisma/migrations/20251016114155_salary_field_is_added/migration.sql
@@ -0,0 +1,9 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `fixedSalary` on the `ScrapedJob` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "ScrapedJob" DROP COLUMN "fixedSalary",
+ADD COLUMN     "salary" TEXT;
